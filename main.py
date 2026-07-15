57dacadd9043f80f0575bd7e1ceb6d956c20acc2
DavidGR20002025 <davidgonroj2000@gmail.com>
2026-07-15 11:14:32 -0600

    Use environment variables for email credentials
    Refactor email credentials to use environment variables and clean up commented code.

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import smtplib
 import random
 import glob
 import pandas as pd
+import os
 
-
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
 now=dt.datetime.now()
 day=now.day
 month=now.month
@@ -18,23 +13,13 @@ df=pd.read_csv("birthdays.csv")
 
 matches=df[(df["day"]==day) & (df["month"]==month)]
 txt_files = glob.glob("letter_templates/*.txt")
-my_email="davidgonroj2000@gmail.com"
-
-
+my_email=os.environ.get("MY_EMAIL")
+my_password=os.environ.get("MY_PASSWORD")
 old_word="[NAME]"
 
-# with open("birthdays.csv") as file:
-#     todays_bithday=[user for user,birthday in csv.reader(file) if birthday==current_day]
-#
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 if not matches.empty:
-     emails=matches.to_dict(orient='records')
-     for person in emails:
-          # print(person["email"])
-          # Find all text files in the current directory
-
+     birthday_people=matches.to_dict(orient='records')
+     for person in birthday_people:
           # Select one file completely at random
           random_file = random.choice(txt_files)
           modified_lines=[]
@@ -46,13 +31,13 @@ if not matches.empty:
           result="".join(modified_lines)
           with smtplib.SMTP("smtp.gmail.com", 587) as connection:
                connection.starttls()
-               connection.login(user=my_email, password="mmmo tvxi gksw gclo")
+               connection.login(user=my_email, password=my_password)
                connection.sendmail(from_addr=my_email,
                                    to_addrs=person["email"],
                                    msg=f"Subject:Happy Birthday\n\n{result}")
 
 else:
      print("No matching days found")
-# 4. Send the letter generated in step 3 to that person's email address.
+
 
 
