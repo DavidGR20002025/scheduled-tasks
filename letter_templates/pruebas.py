2f51a0aa2aca4f46f87ef6c6673ba87ca58bef00
David González <davidgonroj2000@gmail.com>
2026-07-22 10:20:34 -0600

    Organize birthday email project into its own folder

--- letter_templates/pruebas.py
+++
@@ -1,9 +0,0 @@
-# import glob
-# import random
-#
-# # Find all text files in the current directory
-# txt_files = glob.glob("*.txt")
-#
-# # Select one file completely at random
-# random_file = random.choice(txt_files)
-# print(f"Chosen file: {random_file}")
