d1f87b66a32af86c968848371ea10c01bf9516c1
David González <davidgonroj2000@gmail.com>
2026-07-22 10:57:19 -0600

    Use environment variables for API keys and credentials
    Refactored the code to use environment variables for sensitive information like API keys and Twilio credentials.

--- Temp_SMS/main.py
+++ Temp_SMS/main.py
@@ -1,8 +1,9 @@
 import requests
 from twilio.rest import Client
+import os
 
-api_key="NJ4MRKjpKcAdUM5WqCjNaMAP9lnJ4Lues6TKfv0m"
-client_id="jvGY1TZ45MHGdPw9nINrm"
+api_key=os.environ.get("API_KEY")
+client_id=os.environ.get("CLIENT_ID")
 
 
 response=requests.get(f"https://data.api.xweather.com/conditions/new%20york%2C%20ny?client_id={client_id}&client_secret={api_key}")
@@ -10,15 +11,15 @@ response.raise_for_status()
 data=response.json()
 temperature=data["response"][0]["periods"][0]["tempC"]
 
-account_sid = "AC074c585fd709977559b9008854bb7754"
-auth_token = "5718b78ad6cb72d7aeeceba8e09da9dd"
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
 if temperature>18:
     message = client.messages.create(
          body=f"Hello, the weather today is {temperature}",
-         from_="+15734961525",
-         to="+526143671100",
+         from_=os.environ.get("TWILIO_PHONE_NUMBER"),
+         to=os.environ.get("PHONE_NUMBER"),
      )
 else:
     print("All good with temp")
