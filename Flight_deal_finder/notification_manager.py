a020cd2f3535a31b6d960c773884fab4b74e71f6
David González <davidgonroj2000@gmail.com>
2026-08-03 20:06:06 -0600

    Use environment variables for Twilio credentials
    Replaced hardcoded Twilio credentials with environment variables for security.

--- Flight_deal_finder/notification_manager.py
+++ Flight_deal_finder/notification_manager.py
@@ -1,10 +1,11 @@
 from twilio.rest import Client
+import os
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.ACCOUNT_SID = "AC074c585fd709977559b9008854bb7754"
-        self.AUTH_TOKEN = "5718b78ad6cb72d7aeeceba8e09da9dd"
+        self.ACCOUNT_SID = os.environ.get("ACCOUNT_SID")
+        self.AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
         self.client = Client(self.ACCOUNT_SID, self.AUTH_TOKEN)
 
     def send_message(self,price_google,arrival,number_to,number_from,day,new_day):
